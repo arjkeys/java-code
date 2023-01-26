9e453aa70349700d364992a4f77886f12d37594d
Arj Keys <123629116+arjkeys@users.noreply.github.com>
2023-01-26 19:22:47 +0530

    Read contents from a file into byte array - Java

--- read_contents_from_a_file_into_byte_array.java
+++ read_contents_from_a_file_into_byte_array.java
@@ -44,5 +44,5 @@ public class ArjKeys
 }
 /*
 Output:
-Welcome to Arjun Pinpoint. This is a line of text inside the file.
+Providing Java programs for your/student's question. So Don't hesitate to ask question. We'll provide codings for you. So that both can practice well.
 */
