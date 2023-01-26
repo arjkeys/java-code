d722265c8bac992fd279c2917057b63163a6bb88
Arj Keys <123629116+arjkeys@users.noreply.github.com>
2023-01-26 19:41:15 +0530

    Create read_a_file_line_by_line_and_store_it_into_a_variable.java
    Java program to read a file line by line and store it into a variable.

---
+++ read_a_file_line_by_line_and_store_it_into_a_variable.java
@@ -0,0 +1,39 @@
+/*
+Java program to read a file line by line and store it into a variable.
+Note: Rename this filename as Main.java or your wish
+*/
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+ 
+public class ArjunPinpoint
+{ 
+    public static void main(String a[])
+	{
+        StringBuilder sb = new StringBuilder();
+        String strLine = "";
+        String str_data = "";
+        try 
+		{
+            BufferedReader br = new BufferedReader(new FileReader("ArjunPinpoint.txt"));
+            while (strLine != null)
+            {
+                if (strLine == null)
+					break;
+                str_data += strLine;
+                strLine = br.readLine();      
+            }
+            System.out.println(str_data);
+            br.close();
+        } 
+		catch (FileNotFoundException e) 
+		{
+            System.err.println("File not found");
+        } 
+		catch (IOException e) 
+		{
+            System.err.println("Unable to read the file.");
+        }
+     }
+}
