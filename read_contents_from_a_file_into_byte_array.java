9d69a9449e7dd22e228904b593f23b45fccd530d
Arj Keys <123629116+arjkeys@users.noreply.github.com>
2023-01-26 19:22:05 +0530

    Create read_contents_from_a_file_into_byte_array.java
    Read contents from a file into byte array - Java

---
+++ read_contents_from_a_file_into_byte_array.java
@@ -0,0 +1,48 @@
+//Read contents from a file into byte array - Java
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class ArjKeys 
+{
+	public static void main(String a[])
+	{       
+        String file_name = "README.md";
+        InputStream fins = null;
+        try 
+		{
+            fins = new FileInputStream(file_name);
+            byte file_content[] = new byte[2*1024];
+            int read_count = 0;
+            while((read_count = fins.read(file_content)) > 0)
+			{
+                System.out.println(new String(file_content, 0, read_count-1));
+            }
+        } 
+		catch (FileNotFoundException e) 
+		{
+            e.printStackTrace();
+        } 
+		catch (IOException e) 
+		{
+            e.printStackTrace();
+        } 
+		finally 
+		{
+            try
+			{
+                if(fins != null) fins.close();
+            } 
+			catch(Exception ex)
+			{
+                 
+            }
+        }
+    }
+}
+/*
+Output:
+Welcome to Arjun Pinpoint. This is a line of text inside the file.
+*/
