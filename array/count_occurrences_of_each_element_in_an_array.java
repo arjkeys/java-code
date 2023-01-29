03679cfb8d599297956dff6091a1473e6885f1f8
Arj Keys <123629116+arjkeys@users.noreply.github.com>
2023-01-29 15:04:01 +0530

    Create count_occurrences_of_each_element_in_an_array.java
    Count occurrences of each element in an array - Java

---
+++ array/count_occurrences_of_each_element_in_an_array.java
@@ -0,0 +1,37 @@
+/*
+Count occurrences of each element in an array - Java
+Note: Rename this filename as Main.java or your wish
+*/
+import java.util.Arrays;
+import java.util.HashMap;
+public class ArjKeys
+{  
+    static void arrayElementCount(int inputArray[])
+    {
+        //Creating a HashMap object with elements of inputArray as keys and their count as values
+        HashMap<Integer, Integer> elementCountMap = new HashMap<Integer, Integer>();
+        //checking every element of the inputArray
+        for (int i : inputArray) 
+        {
+            if(elementCountMap.containsKey(i))
+            {
+                //If element is present in elementCountMap, incrementing it's count by 1
+                elementCountMap.put(i, elementCountMap.get(i)+1);
+            }
+            else
+            {
+                //If element is not present in elementCountMap, 
+                //adding this element to elementCountMap with 1 as it's value         
+                elementCountMap.put(i, 1);
+            }
+        }
+        System.out.println("Input Array : "+Arrays.toString(inputArray));
+        System.out.println("Element Count : "+elementCountMap);
+    }
+    public static void main(String[] args) 
+    {    
+        arrayElementCount(new int[]{4, 5, 4, 5, 4, 6});
+        arrayElementCount(new int[]{12, 9, 12, 9, 10, 9, 10, 11});
+        arrayElementCount(new int[]{891, 187, 891, 187, 891, 476, 555, 741});
+    }    
+}
